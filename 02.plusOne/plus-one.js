255c20e425bdf8a5fef3c579e100584d84d99c82
maunga <maungamulomba@outlook.com>
2023-10-30 09:30:32 +0200

    correct code for 02.plusOne

--- 02.plusOne/plus-one.js
+++ 02.plusOne/plus-one.js
@@ -1,8 +1,16 @@
 let plusOne = function (digits) {
-  let joinNums = digits.join("");
-  let singleDigit = parseInt(joinNums);
-  singleDigit += 1;
-  let newDigits = String(singleDigit).split("");
+  let carry = 1; // Initialize the carry to 1, as we want to add one.
+  let result = [];
 
-  return newDigits;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let sum = digits[i] + carry;
+    result.unshift(sum % 10); // Add the current digit to the result array.
+    carry = Math.floor(sum / 10); // Calculate the carry for the next iteration.
+  }
+
+  if (carry > 0) {
+    result.unshift(carry); // If there's a carry left, add it as a new digit.
+  }
+
+  return result;
 };
